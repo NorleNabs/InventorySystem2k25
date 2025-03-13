4760305222507ec1d0bb5f34f82f3b47758eae66
NorleNabs <162889054+NorleNabs@users.noreply.github.com>
2025-03-13 16:40:25 +0800

    Create inventorysystem2k25.sql

---
+++ inventorysystem2k25.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 13, 2025 at 09:39 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `inventorysystem2k25`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `view_inventory`
+--
+
+CREATE TABLE `view_inventory` (
+  `itemID` int(11) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `category` int(255) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `view_inventory`
+--
+ALTER TABLE `view_inventory`
+  ADD PRIMARY KEY (`itemID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `view_inventory`
+--
+ALTER TABLE `view_inventory`
+  MODIFY `itemID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
